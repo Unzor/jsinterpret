b0e81c528a520d4a913331655348d96a67aa1d05
Unzor <71789103+Unzor@users.noreply.github.com>
2021-07-19 22:17:45 -0500

    Add main files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # jsinterpret
-A JavaScript interpreter for Node.js.
+A JavaScript interpreter inside of NodeJS.
+
+# Usage
+```javascript
+var jsinterpret=require('jsinterpret');
+
+jsinterpret.interpret("console.log('Hello World!');", function(e){
+  console.log(e);
+});
+```
+To require() a package, use jsinterpret.addPackage(packageName) before any script, or install it in your machine.
+Example:
+
+```javascript
+var jsinterpret=require('jsinterpret');
+
+jsinterpret.addPackage("chalk")
+
+jsinterpret.interpret(`const chalk=require('chalk'); 
+console.log(chalk.blue('Hello World!'));`, function(e){
+  console.log(e);
+});
+```
---
+++ index.js
@@ -0,0 +1,182 @@
+const shell=require('shelljs');
+
+exports.interpret=function(__code__, callback){
+var return_log=function(a){callback(a);}
+            function replaceAll(string, search, replace) {
+  return string.split(search).join(replace);
+}
+
+const util = require("util");
+const repl = require("repl");
+const path = require("path");
+const fs = require("fs");
+const vm = require("vm");
+const rl = require("./readline-sync.js");
+const Module = require("module");
+
+let r;
+
+// Red errors.
+function logError(msg) {
+  return_log(msg);
+}
+
+// The nodejs repl operates in raw mode and does some funky stuff to
+// the terminal. This ns the repl and forces non-raw mode.
+function pauseRepl() {
+  if (!r) return;
+
+  r.pause();
+  process.stdin.setRawMode(false);
+}
+
+// Forces raw mode and resumes the repl.
+function resumeRepl() {
+  if (!r) return;
+
+  process.stdin.setRawMode(true);
+  r.resume();
+}
+
+// Clear the line if it has anything on it.
+function clearLine() {
+  if (r && r.line) r.clearLine();
+}
+
+// Adapted from the internal node repl code just a lot simpler and adds
+// red errors (see https://bit.ly/2FRM86S)
+function handleError(e) {
+  if (r) {
+    r.lastError = e;
+  }
+
+  if (e && typeof e === "object" && e.stack && e.name) {
+    if (e.name === "SyntaxError") {
+      e.stack = e.stack
+        .replace(/^repl:\d+\r?\n/, "")
+        .replace(/^\s+at\s.*\n?/gm, "");
+    }
+
+    logError(e.stack);
+  } else {
+    // For some reason needs a newline to flush.
+    logError("Thrown: " + r.writer(e) + "\n");
+  }
+
+  if (r) {
+    r.clearBufferedCommand();
+    r.lines.level = [];
+    r.displayPrompt();
+  }
+}
+
+function start(context) {
+  r = repl.start({
+    prompt: process.env.PRYBAR_PS1,
+    useGlobal: true,
+  });
+
+  // remove the internal error and ours for red etc.
+  r._domain.removeListener("error", r._domain.listeners("error")[0]);
+  r._domain.on("error", handleError);
+  process.on("uncaughtException", handleError);
+}
+
+global.alert = return_log;
+global.require=require;
+global.prompt = (p) => {
+  pauseRepl();
+  clearLine();
+
+  let ret = rl.question(`${p}> `, {
+    hideEchoBack: false,
+  });
+
+  resumeRepl();
+
+  // Display prompt on the next turn.
+  if (r) setImmediate(() => r.displayPrompt());
+
+  return ret;
+};
+
+global.confirm = (q) => {
+  pauseRepl();
+  clearLine();
+
+  const ret = rl.keyInYNStrict(q);
+
+  resumeRepl();
+
+  // Display prompt on the next turn.
+  if (r) setImmediate(() => r.displayPrompt());
+  return ret;
+};
+
+if (__code__) {
+  vm.runInThisContext(__code__);
+  if (process.env.PRYBAR_INTERACTIVE) {
+    start();
+  }
+} else if (process.env.PRYBAR_EXP) {
+ return_log(vm.runInThisContext(process.env.PRYBAR_EXP));
+  if (process.env.PRYBAR_INTERACTIVE) {
+    start();
+  }
+} else if (process.env.PRYBAR_FILE) {
+  const mainPath = path.resolve(process.env.PRYBAR_FILE);
+  const main = fs.readFileSync(mainPath, "utf-8");
+  const module = new Module(mainPath, null);
+
+  module.id = ".";
+  module.filename = mainPath;
+  module.paths = Module._nodeModulePaths(path.dirname(mainPath));
+
+  process.mainModule = module;
+
+  global.module = module;
+  global.require = module.require.bind(module);
+  global.__dirname = path.dirname(mainPath);
+  global.__filename = mainPath;
+
+  let script;
+  try {
+    script = vm.createScript(__code__, {
+      filename: mainPath,
+      displayErrors: false,
+        require: require,
+  console: console
+    });
+  } catch (e) {
+    handleError(e);
+  }
+
+  if (script) {
+    let res;
+    try {
+      res = script.runInThisContext({
+        displayErrors: false,
+      });
+     } catch (e) {
+      handleError(e);
+    }
+
+    module.loaded = true;
+
+    if (typeof res !== "undefined") {
+      return_log(util.inspect(res, { colors: true }));
+    }
+  }
+
+  if (process.env.PRYBAR_INTERACTIVE) {
+    process.once("SIGINT", () => start());
+    process.once("beforeExit", () => start());
+  }
+} else if (process.env.PRYBAR_INTERACTIVE) {
+  start();
+}
+}
+
+exports.addPackage=function(a){
+shell.exec('npm install ' + a);
+}
