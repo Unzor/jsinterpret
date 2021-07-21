3798f1e734f655d4e223adf3ef94163fab785bcd
Unzor <71789103+Unzor@users.noreply.github.com>
2021-07-21 15:00:24 -0500

    Update jsinterpret_browser.js

--- browser/jsinterpret_browser.js
+++ browser/jsinterpret_browser.js
@@ -1,6 +1,6 @@
-var jsinterpret={};
+var jsinterpret_array={}; class JSInterpret{constructor(){return jsinterpret_array}}
 
-jsinterpret.interpret_js=function(a___d_interp, callback){
+jsinterpret_array.interpret=function(a___d_interp, callback){
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (process){(function (){ if (callback){console.log=function(a){callback(a);}}
 require('vm').runInNewContext(a___d_interp, {process: process, console: console, require: require});
